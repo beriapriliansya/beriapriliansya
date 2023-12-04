d124f3f15a5be74465d87b2f576a7e6df245a405
Berryapr <147056747+beriapriliansya@users.noreply.github.com>
2023-12-04 21:35:12 +0700

    manage.php

---
+++ manage.php
@@ -0,0 +1,51 @@
+<div class="col-lg-8">
+    <div class="card">
+        <div class="card-header">
+            <b>Welcome to My Blog</b>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Manage Artikel</h5>
+             <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Judul</th>
+                        <th>Isi</th>
+                        <th>Kategori</th>
+                        <th>action</th>
+                    </tr>
+                </thead>
+                <?php
+                    include "koneksi.php";
+                    $data = $conn->query("select * from tbl_blog");
+                ?>
+                <tbody>
+                    <?php
+                        $no =1;
+                        while($row=$data->fetch_assoc()) {
+                    ?>
+                    <tr>
+                        <td><?php echo $no ?></td>
+                        <td><?php echo $row['judul']; ?></td>
+                        <td><?php echo $row['isi']; ?></td>
+                        <td><?php echo $row['kategori']; ?></td>
+                        <td>
+
+                    <a href="hapus.php?id_hapus=<?php echo $row['id'] ?>">Hapus</a>
+
+                    |
+
+                    <a href="?menu=4&id_edit=<?php echo $row['id'] ?>">Edit</a>
+
+                    </td>
+                    </tr>
+                    <?php 
+                        $no++;
+                        }
+                    ?>
+
+                </tbody>
+             </table>   
+        </div>
+    </div>
+</div>
